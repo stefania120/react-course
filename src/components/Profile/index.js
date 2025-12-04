4ee2e68a92bb0aa60805e94e040da926af2e2553
Stefania Formato <sformato95@gmail.com>
2025-12-04 21:42:53 +0100

    Add Lyfecycle method

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,9 +1,39 @@
 import React from "react";
 
 class Profile extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+
+  static getDerivedStateFromProps() {
+    console.log("getDerivedStateFromProps");
+  }
+  
+  shouldComponentUpdate() {
+    if(this.props.count > 100){
+        return true;
+    }
+    return false;
+  }
+
+  getSnapshotBeforeUpdate(){
+
+  }
+
+  componentDidUpdate(){
+
+  }
+
   render() {
     return <h1>Profile - {this.props.username}</h1>;
   }
+
+    componentDidMount() {
+    console.log("componentDidMount");
+  }
 }
+
 
 export default Profile;
