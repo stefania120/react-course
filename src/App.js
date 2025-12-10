8d4dab01ea98f0948f6271300389dff2782c986f
Stefania Formato <sformato95@gmail.com>
2025-12-10 23:53:06 +0100

    Add UsersTable

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, use } from "react";
 import "./App.css";
 import {
   Button,
   TextField,
-  FormControlLabel,
-  Checkbox,
-  FormGroup,
-  InputLabel,
-  MenuItem,
-  FormControl,
-  Select,
-  CircularProgress,
-  Grid
+  Grid,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+  Table
 } from "@mui/material";
 
 const App = () => {
 
+  const [users, setUsers] = useState([]);
+
   return (
     <>
-    <Form></Form>
+      <Form/>
+      <hr></hr>
+      <UsersTable users={users}/>
     </>
   );
 };
@@ -32,49 +35,134 @@ const Form = () => {
     province: "",
     street: "",
     streetNumber: "",
-    postalCode: ""
+    postalCode: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
-  }
+  };
 
-  return(
+  return (
     <Grid container spacing={3} justifyContent="flex-start" alignItems="center">
       <Grid item xs={12} sm={6} lg={4}>
-        <TextField fullWidth name="fullname" onChange={handleChange} value={formData.fullname} variant="outlined" label="Fullname"/>
+        <TextField
+          fullWidth
+          name="fullname"
+          onChange={handleChange}
+          value={formData.fullname}
+          variant="outlined"
+          label="Fullname"
+        />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
-        <TextField fullWidth name="email" onChange={handleChange} value={formData.email} variant="outlined" label="Email"/>
+        <TextField
+          fullWidth
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
+          variant="outlined"
+          label="Email"
+        />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
-        <TextField fullWidth name="password" onChange={handleChange} value={formData.password} variant="outlined" label="Password" type="password"/>
-      </Grid>
-            <Grid item xs={12} sm={6} lg={4}>
-        <TextField fullWidth name="city" onChange={handleChange} value={formData.city} variant="outlined" label="City"/>
-      </Grid>
-            <Grid item xs={12} sm={6} lg={4}>
-        <TextField fullWidth name="province" onChange={handleChange} value={formData.province} variant="outlined" label="Province"/>
+        <TextField
+          fullWidth
+          name="password"
+          onChange={handleChange}
+          value={formData.password}
+          variant="outlined"
+          label="Password"
+          type="password"
+        />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
-        <TextField fullWidth name="postalCode" onChange={handleChange} value={formData.postalCode} variant="outlined" label="PostalCode"/>
+        <TextField
+          fullWidth
+          name="city"
+          onChange={handleChange}
+          value={formData.city}
+          variant="outlined"
+          label="City"
+        />
       </Grid>
-            <Grid item xs={12} sm={6} lg={4}>
-        <TextField fullWidth name="street" onChange={handleChange} value={formData.street} variant="outlined" label="Street"/>
+      <Grid item xs={12} sm={6} lg={4}>
+        <TextField
+          fullWidth
+          name="province"
+          onChange={handleChange}
+          value={formData.province}
+          variant="outlined"
+          label="Province"
+        />
       </Grid>
-            <Grid item xs={12} sm={6} lg={4}>
-        <TextField fullWidth name="streetNumber" onChange={handleChange} value={formData.streetNumber} variant="outlined" label="StreetNumber"/>
+      <Grid item xs={12} sm={6} lg={4}>
+        <TextField
+          fullWidth
+          name="postalCode"
+          onChange={handleChange}
+          value={formData.postalCode}
+          variant="outlined"
+          label="PostalCode"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6} lg={4}>
+        <TextField
+          fullWidth
+          name="street"
+          onChange={handleChange}
+          value={formData.street}
+          variant="outlined"
+          label="Street"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6} lg={4}>
+        <TextField
+          fullWidth
+          name="streetNumber"
+          onChange={handleChange}
+          value={formData.streetNumber}
+          variant="outlined"
+          label="StreetNumber"
+        />
       </Grid>
     </Grid>
+  );
+};
+
+const UsersTable = (props) => {
+  return(
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Fullname</TableCell>
+            <TableCell >Email</TableCell>
+            <TableCell >Address</TableCell>
+            {/* <TableCell align="right">Delete</TableCell> */}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.users.map((user, i) => (
+            <TableRow
+              key={i}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {user.fullname}
+              </TableCell>
+              <TableCell >{user.email}</TableCell>
+              <TableCell align="right">{`${user.street} ${user.streetNumber} ${user.city} (${user.province}) - ${user.postalCode}`}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
-}
-
-const Table = () => {
-
-}
+};
 
 export default App;
